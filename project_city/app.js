64391af8430424269de9db4dc370bff2194120e1
Adam966 <adam.ivan@akademiasovy.sk>
2018-12-10 13:49:35 +0100

    table added

---
+++ project_city/app.js
@@ -0,0 +1,6 @@
+function sendData() {
+  let city = document.getElementById('city').value;
+  let cCode = document.getElementById('cCode').value;
+  console.log(city);
+  console.log(cCode);
+}
---
+++ project_city/readme.txt
@@ -0,0 +1,2 @@
+table with town and code of country 
+response from server is number of citizens, geo cash with google map, weather information
